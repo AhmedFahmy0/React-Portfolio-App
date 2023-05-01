8fa8403281dbadbfa8cd531f453e08564c80bc91
Ahmed Fahmy <106992620+AhmedFahmy0@users.noreply.github.com>
2023-05-01 21:22:43 +0200

    w

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -14,7 +14,7 @@ import UpComing from './Movies/UpComing/UpComing';
 
 const router = createHashRouter([
   {
-    path: '/first-react', element: <Layout />, children: [
+    path: '/', element: <Layout />, children: [
       { index: true, element: <Home /> },
       { path: 'portfolio', element: <Portfolio /> },
       { path: 'about', element: <About /> },
